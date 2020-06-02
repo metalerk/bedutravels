7982f60eb3d8e333358d3e3c5d1154d172d13022
Luis Esteban <rodriguezjluis0@gmail.com>
2020-06-01 22:01:24 -0500

    update admin

--- tours/admin.py
+++ tours/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
+from .models import (
+    Tour,
+    User,
+    Zone,
+)
 
-# Register your models here.
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        'name', 
+        'last_name', 
+        'nickname', 
+        'email', 
+        'gender', 
+        'birthday',
+    )
+
+
+class TourAdmin(admin.ModelAdmin):
+    list_display = (
+        'pk',
+        'name',
+        'operator', 
+        'slug', 
+        'tour_type', 
+        'description', 
+        'country', 
+        'takeoff_zone', 
+        'arrival_zone',
+    )
+
+
+class ZoneAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name', 'description', 'latitude', 'longitude',)
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Tour, TourAdmin)
+admin.site.register(Zone, ZoneAdmin)
