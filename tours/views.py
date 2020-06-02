3d8d3bab13d9d14e020a893f4702e40a2ed62345
Luis Esteban <rodriguezjluis0@gmail.com>
2020-06-01 22:01:30 -0500

    update views

--- tours/views.py
+++ tours/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import Tour
 
 def index(request):
-    return render(request, 'tours/index.html')
+    tours = Tour.objects.all()
+    return render(request, 'tours/index.html', {'tours': tours, 'user': request.user})
