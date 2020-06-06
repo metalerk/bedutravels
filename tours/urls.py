7f1de0361524c91f6d852a7050242e1070c69c1e
Luis Esteban <rodriguezjluis0@gmail.com>
2020-06-06 10:49:07 -0500

    update urls.py

--- tours/urls.py
+++ tours/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from .views import (
     index,
+    login_user,
+    logout_user,
 )
 
 urlpatterns = [
+    path('login/', login_user, name='login_user'),
+    path('logout/', logout_user, name='logout_user'),
     path('', index, name='index'),
 ]
