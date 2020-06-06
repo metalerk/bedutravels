c9be12b356f8e8dc0d27f93a56fb0b992ecc5d04
Luis Esteban <rodriguezjluis0@gmail.com>
2020-06-06 09:56:09 -0500

    add dynamic arrivals render

--- tours/views.py
+++ tours/views.py
@@ -3,4 +3,13 @@ from .models import Tour
 
 def index(request):
     tours = Tour.objects.all()
-    return render(request, 'tours/index.html', {'tours': tours, 'user': request.user})
+    return render(
+        request,
+        'tours/index.html',
+        {
+            'tours': tours,
+            'tour_arrivals': tours.distinct().values_list('arrival_zone__name', flat=True),
+            'tour_takeoff': tours.distinct().values_list('takeoff_zone__name', flat=True),
+            'user': request.user,
+        }
+    )
