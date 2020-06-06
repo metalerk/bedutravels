76b00ca941549141a00c0a911af3218be886ce88
Luis Esteban <rodriguezjluis0@gmail.com>
2020-06-06 10:49:13 -0500

    update views.py

--- tours/views.py
+++ tours/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
 from .models import Tour
 
+@login_required()
 def index(request):
     tours = Tour.objects.all()
     return render(
@@ -13,3 +16,31 @@ def index(request):
             'user': request.user,
         }
     )
+
+def login_user(request):
+    
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        _next = request.GET.get('next', '/')
+        access = authenticate(username=username, password=password)
+        if access is not None:
+            login(request, access)
+            return redirect(_next)
+        else:
+            msg = 'Bad credentials!'
+    else:
+        msg = ''
+    
+    return render(
+        request,
+        'registration/login.html',
+        {
+            'msg': msg,
+        }
+    )
+
+def logout_user(request):
+    _next = request.GET.get('next', '/')
+    logout(request)
+    return redirect(_next)
