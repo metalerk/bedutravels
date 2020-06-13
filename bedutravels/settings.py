8b0810e30394b65d0c644f6f891494477e4506d2
Luis Esteban <rodriguezjluis0@gmail.com>
2020-06-13 11:52:52 -0500

    update settings

--- bedutravels/settings.py
+++ bedutravels/settings.py
@@ -30,17 +30,27 @@ ALLOWED_HOSTS = ['*']
 
 # Application definition
 
-INSTALLED_APPS = [
+DJANGO_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_extensions',
+]
+
+LOCAL_APPS = [
+    'profiles',
     'tours',
+]
+
+THIRD_PARTY_APPS = [
+    'django_extensions',
+    'graphene_django',
     'rest_framework',
 ]
+
+INSTALLED_APPS = DJANGO_APPS + LOCAL_APPS + THIRD_PARTY_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
